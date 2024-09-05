c21e459ccbf0ceb0fa7d683a92f621609600ecff
Daniel <90870585+VexyPT@users.noreply.github.com>
2024-09-05 21:38:31 +0100

    Fix

--- src/commands/slash/moderation/tempRoleCommand.js
+++ src/commands/slash/moderation/tempRoleCommand.js
@@ -103,7 +103,6 @@ module.exports = {
                   return interaction.reply({ embeds: [embedError], ephemeral: true });
                 }
 
-                // Ajuste para lidar com "1y" e definir limites
                 const milliseconds = ms(time) || (time == "1y" ? parseInt(time) * 365 * 24 * 60 * 60 * 1000 : null);
                 if (!milliseconds || milliseconds <= 0) {
                   embedError.setDescription(t("tempRole.invalidTime", {
@@ -115,7 +114,7 @@ module.exports = {
                   return interaction.reply({ embeds: [embedError], ephemeral: true });
                 }
 
-                const premiumUser = userdb.premium
+                const premiumUser = userdb.premium;
                 const maxDuration = premiumUser ? 366 * 24 * 60 * 60 * 1000 : 31 * 24 * 60 * 60 * 1000;
 
                 if (milliseconds > maxDuration) {
@@ -130,7 +129,6 @@ module.exports = {
 
                 const expiresAt = new Date(Date.now() + milliseconds);
 
-                // Verificar se o cargo é gerenciável
                 if (!guild.roles.cache.get(role.id).editable) {
                   embedError.setDescription(t("tempRole.notEditable", {
                     locale: language,
@@ -141,7 +139,6 @@ module.exports = {
                   return interaction.reply({ embeds: [embedError], ephemeral: true });
                 }
 
-                // Verificar se o cargo já existe no usuário
                 const memberToAssign = await guild.members.fetch(user.id);
                 if (memberToAssign.roles.cache.has(role.id)) {
                   embedError.setDescription(t("tempRole.alreadyHaveTheRole", {
@@ -166,7 +163,6 @@ module.exports = {
 
                 await tempRole.save();
 
-                // Emitir o evento para que o sistema de verificação atualize
                 interaction.client.emit('tempRoleUpdate');
 
                 const embedSuccess = new EmbedBuilder({
@@ -304,7 +300,245 @@ module.exports = {
                 break;
             }
 
+            case "manage": {
+              const user = options.getUser("user", true);
             
+              if (!member.permissions.has(PermissionFlagsBits.ManageRoles)) {
+                embedError.setDescription(t("permissions.meMissingManageRolesPermission", {
+                  locale: language,
+                  replacements: {
+                    denyEmoji: e.deny
+                  }
+                }));
+                return interaction.reply({ embeds: [embedError], ephemeral: true });
+              }
+            
+              const tempRoles = await TempRole.find({ guildID: guild.id, userID: user.id });
+            
+              if (!tempRoles.length) {
+                embedError.setDescription(t("tempRole.noRolesForUser", {
+                  locale: language,
+                  replacements: {
+                    denyEmoji: e.deny,
+                    user
+                  }
+                }));
+                return interaction.reply({ embeds: [embedError], ephemeral: true });
+              }
+            
+              const selectMenu = new StringSelectMenuBuilder({
+                customId: "tempRole_manage_select",
+                placeholder: t("tempRole.manage.manageSelect.placeholder", { locale: language }),
+                options: tempRoles.map(role => {
+                  const guildRole = guild.roles.cache.get(role.roleID);
+                  const roleName = guildRole ? guildRole.name : "Cargo não encontrado";
+            
+                  return {
+                    label: roleName,
+                    value: role.id
+                  };
+                })
+              });
+            
+              const actionRow = new ActionRowBuilder().addComponents(selectMenu);
+            
+              const embed = new EmbedBuilder({
+                color: color.default,
+                title: t("tempRole.manage.title", { locale: language }),
+                description: t("tempRole.manage.description", {
+                  locale: language,
+                  replacements: { user }
+                })
+              });
+            
+              await interaction.reply({
+                embeds: [embed],
+                components: [actionRow],
+                ephemeral: true
+              });
+            
+              const collector = interaction.channel.createMessageComponentCollector({
+                componentType: ComponentType.StringSelect,
+                time: 120000
+              });
+            
+              collector.on('collect', async i => {
+            
+                const selectedRoleId = i.values[0];
+                const selectedRole = tempRoles.find(role => role.id === selectedRoleId);
+            
+                if (!selectedRole) {
+                  return i.reply({ content: t("tempRole.manage.invalidRole", { locale: language }), ephemeral: true });
+                }
+            
+                const actionButtons = new ActionRowBuilder().addComponents(
+                  new ButtonBuilder()
+                    .setCustomId(`tempRole_manage_remove_${selectedRole.id}`)
+                    .setLabel(t("tempRole.manage.manageRemove.label", { locale: language }))
+                    .setStyle(ButtonStyle.Danger),
+                  new ButtonBuilder()
+                    .setCustomId(`tempRole_manage_edit_${selectedRole.id}`)
+                    .setLabel(t("tempRole.manage.manageEdit.label", { locale: language }))
+                    .setStyle(ButtonStyle.Primary)
+                );
+            
+                const roleEmbed = new EmbedBuilder({
+                  color: color.default,
+                  title: t("tempRole.manage.manageSelected.title", { locale: language }),
+                  description: t("tempRole.manage.manageSelected.description", {
+                    locale: language,
+                    replacements: {
+                      roleID: selectedRole.roleID,
+                      expiresAt: Math.floor(selectedRole.expiresAt.getTime() / 1000)
+                    }
+                  })
+                });
+            
+                await i.update({
+                  embeds: [roleEmbed],
+                  components: [actionButtons]
+                });
+            
+                const buttonCollector = i.channel.createMessageComponentCollector({
+                  componentType: ComponentType.Button,
+                  time: 120000
+                });
+            
+                buttonCollector.on('collect', async buttonInteraction => {
+                  const customId = buttonInteraction.customId;
+            
+                  if (customId.startsWith("tempRole_manage_remove_")) {
+                    const selectedRoleId = customId.split("_").pop();
+                    await TempRole.findByIdAndDelete(selectedRoleId);
+            
+                    const memberToRemoveRole = await guild.members.fetch(user.id);
+                    const roleToRemove = guild.roles.cache.get(tempRoles.find(role => role.id === selectedRoleId)?.roleID);
+            
+                    if (roleToRemove) {
+                      try {
+                        await memberToRemoveRole.roles.remove(roleToRemove);
+                      } catch {
+                        const embedError = new EmbedBuilder({
+                          color: color.danger,
+                          description: t("tempRole.manage.invalidRole", {
+                            locale: language,
+                            replacements: {
+                              denyEmoji: e.deny
+                            }
+                          }),
+                        });
+                
+                        await buttonInteraction.update({
+                          embeds: [embedError],
+                          components: []
+                        });
+                      }
+                    }
+            
+                    const embedSuccess = new EmbedBuilder({
+                      color: color.success,
+                      description: t("tempRole.manage.removedSuccess.description", {
+                        locale: language,
+                        replacements: {
+                          checkEmoji: e.check,
+                          roleToRemove,
+                          user
+                        }
+                      }),
+                    });
+            
+                    await buttonInteraction.update({
+                      embeds: [embedSuccess],
+                      components: []
+                    });
+                  } else if (customId.startsWith("tempRole_manage_edit_")) {
+                    const selectedRoleId = customId.split("_").pop();
+                    const modal = new ModalBuilder()
+                      .setCustomId(`tempRole_manage_edit_modal_${selectedRoleId}`)
+                      .setTitle(t("tempRole.manage.manageEdit.modalTitle", { locale: language }));
+            
+                    const timeInput = new TextInputBuilder()
+                      .setCustomId("tempRole_manage_edit_time")
+                      .setLabel(t("tempRole.manage.manageEdit.modalTimeLabel", { locale: language }))
+                      .setStyle(TextInputStyle.Short)
+                      .setPlaceholder("e.g., 5m, 3h, 1d, 1y")
+                      .setRequired(true);
+            
+                    const timeInputRow = new ActionRowBuilder().addComponents(timeInput);
+            
+                    modal.addComponents(timeInputRow);
+            
+                    try {
+                      await buttonInteraction.showModal(modal);
+                    } catch (error) {
+                      console.error("Error showing modal:", error);
+                      await buttonInteraction.reply({ content: "Ocorreu um erro ao abrir o modal.", ephemeral: true });
+                    }
+                  }
+                });
+            
+                buttonCollector.on('end', () => {
+                  i.editReply({ components: [] });
+                });
+              });
+            
+              collector.on('end', () => {
+                interaction.editReply({ components: [] });
+              });
+
+              interaction.client.on("interactionCreate", async modalInteraction => {
+                if (!modalInteraction.isModalSubmit()) return;
+            
+                const customId = modalInteraction.customId;
+            
+                if (customId.startsWith("tempRole_manage_edit_modal_")) {
+                  const selectedRoleId = customId.split("_").pop();
+                  const newTime = modalInteraction.fields.getTextInputValue("tempRole_manage_edit_time");
+                
+                  const milliseconds = ms(newTime) || (time == "1y" ? parseInt(time) * 365 * 24 * 60 * 60 * 1000 : null);
+                  if (!milliseconds || milliseconds <= 0) {
+                    embedError.setDescription(t("tempRole.invalidTime", {
+                      locale: language,
+                      replacements: {
+                        denyEmoji: e.deny
+                      }
+                    }));
+                    return interaction.reply({ embeds: [embedError], ephemeral: true });
+                  }
+
+                  const premiumUser = userdb.premium;
+                  const maxDuration = premiumUser ? 366 * 24 * 60 * 60 * 1000 : 31 * 24 * 60 * 60 * 1000;
+
+                  if (milliseconds > maxDuration) {
+                    embedError.setDescription(t("tempRole.durationExceedsLimit", {
+                      locale: language,
+                      replacements: {
+                        denyEmoji: e.deny
+                      }
+                    }));
+                    return interaction.reply({ embeds: [embedError], ephemeral: true });
+                  }
+            
+                  const tempRole = await TempRole.findById(selectedRoleId);
+            
+                  if (!tempRole) {
+                    return modalInteraction.reply({ content: "Cargo temporário não encontrado.", ephemeral: true });
+                  }
+            
+                  const expiresAt = new Date(Date.now() + milliseconds);
+                  tempRole.expiresAt = expiresAt;
+                  await tempRole.save();
+                  interaction.client.emit('tempRoleUpdate');
+            
+                  await modalInteraction.reply({
+                    content: `Cargo temporário atualizado para expirar em <t:${Math.floor(expiresAt.getTime() / 1000)}:F>.`,
+                    ephemeral: true
+                  });
+                }
+              });
+
+              break;
+            }
             
         }
     }
