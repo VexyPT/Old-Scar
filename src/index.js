0793b45ddcc7ae009c8e762903414baff49d67ff
Daniel <90870585+VexyPT@users.noreply.github.com>
2024-09-15 13:33:59 +0100

    anti crash

--- src/index.js
+++ src/index.js
@@ -24,8 +24,25 @@ handler(client);
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    client.login(process.env.TOKEN);
+    client.login(process.env.TOKEN); // login
   })
   .catch(err => console.error("Failed to connect to MongoDB", err));
 
 module.exports = { client };
+
+process.on("unhandledRejection", (reason, p) => {
+  console.log("[antiCrash] :: Unhandled Rejection/Catch" + reason);
+  console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log("[antiCrash] :: Uncaught Exception/Catch" + err.message);
+  console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log("[antiCrash] :: Uncaught Exception/Catch (MONITOR)" + err.message);
+  console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log("[antiCrash] :: Multiple Resolves" + reason);
+  console.log(type, promise, reason);
+});
