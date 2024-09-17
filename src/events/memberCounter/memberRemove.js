00c085f57ef4901da0f1ffd4041fdcf018495585
Daniel <90870585+VexyPT@users.noreply.github.com>
2024-09-17 02:44:28 +0100

    :8ball: | Update

--- src/events/memberCounter/memberRemove.js
+++ src/events/memberCounter/memberRemove.js
@@ -4,19 +4,19 @@ module.exports = {
   name: "guildMemberRemove",
   customName: "memberCountRemove",
   async execute (member) {
-    const guildID = member.guild.id;
-        const today = new Date();
-        today.setHours(0, 0, 0, 0); // Normaliza a data para o início do dia
 
-        try {
-            // Atualiza ou cria o documento para o dia atual
-            await MemberCount.findOneAndUpdate(
-                { guildID, date: today },
-                { $inc: { leaves: 1 } },
-                { upsert: true, new: true }
-            );
-        } catch (error) {
-            console.error('Erro ao atualizar contagem de membros que saíram:', error);
-        }
+    const guildID = member.guild.id;
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    try {
+      await MemberCount.findOneAndUpdate(
+        { guildID, date: today },
+        { $inc: { leaves: 1 } },
+        { upsert: true, new: true }
+      );
+    } catch (error) {
+      console.error(`Erro ao atualizar contagem de membros que saíram no servidor ${guildID}:`, error);
+    }
   }
 }
--- src/models/MemberCountSettings.js
+++ src/models/MemberCountSettings.js
@@ -4,7 +4,9 @@ const MemberCountSchema = new Schema({
     guildID: { type: String, required: true },
     date: { type: Date, required: true },
     joins: { type: Number, default: 0 },
-    leaves: { type: Number, default: 0 }
+    leaves: { type: Number, default: 0 },
+    totalHumanMembers: { type: Number, required: true },
+    totalBotsMembers: { type: Number, required: true },
 });
 
 MemberCountSchema.index({ guildID: 1, date: 1 }, { unique: true });
